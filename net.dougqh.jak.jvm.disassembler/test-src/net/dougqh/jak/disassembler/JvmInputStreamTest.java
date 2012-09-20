ac9ad8ac5dad91e11c9401de66fc78deee7d49d1
Douglas Q Hawkins <dougqh@gmail.com>
2012-09-20 19:08:04 -0400

    Adding more tests to JvmInputStream

--- net.dougqh.jak.jvm.disassembler/test-src/net/dougqh/jak/disassembler/JvmInputStreamTest.java
+++ net.dougqh.jak.jvm.disassembler/test-src/net/dougqh/jak/disassembler/JvmInputStreamTest.java
@@ -12,7 +12,7 @@ import static org.hamcrest.CoreMatchers.*;
 public final class JvmInputStreamTest {
 	@Test
 	public final void readSingleBlock() throws EOFException {
-		JvmInputStream in = new JvmInputStream(new byte[] { 0, 1 });
+		JvmInputStream in = new JvmInputStream(new byte[]{ 0, 1 });
 		assertThat( in.u1(), isByte(0) );
 		assertThat( in.u1(), isByte(1) );
 		assertThat( in.isDone(), is(true) );
@@ -21,14 +21,47 @@ public final class JvmInputStreamTest {
 	@Test
 	public final void readMultiBlock() throws EOFException {
 		JvmInputStream in = new JvmInputStream(
-			new byte[] { 0, 1 },
-			new byte[] { 2, 3 }
+			new byte[]{ 0, 1 },
+			new byte[]{ 2, 3 }
 		);
 		
 		assertThat( in.u1(), isByte(0) );
 		assertThat( in.u1(), isByte(1) );
 		assertThat( in.u1(), isByte(2) );
 		assertThat( in.u1(), isByte(3) );
+		assertThat( in.isDone(), is(true) );
+	}
+	
+	@Test
+	public final void readBytesSingleBlock() throws EOFException {
+		JvmInputStream in = new JvmInputStream(new byte[]{ 0, 1 });
+		assertThat( in.u1(), isByte(0) );
+		assertThat( in.u1(), isByte(1) );
+		assertThat( in.isDone(), is(true) );
+	}
+	
+	@Test
+	public final void readBytesWithinBlock() throws EOFException {
+		JvmInputStream in = new JvmInputStream(new byte[]{ 0, 1, 2, 3 });
+		assertThat( in.readBytes(3), is(new byte[]{ 0, 1, 2 }) );
+		assertThat( in.isDone(), is(false) );
+	}
+	
+	@Test
+	public final void readBytesAllOfBlock() throws EOFException {
+		JvmInputStream in = new JvmInputStream(new byte[]{ 0, 1, 2, 3 });
+		assertThat( in.readBytes(4), is(new byte[]{ 0, 1, 2, 3 }) );
+		assertThat( in.isDone(), is(true) );
+	}
+	
+	@Test
+	public final void readBytesMultiBlock() throws EOFException {
+		JvmInputStream in = new JvmInputStream(
+			new byte[]{ 0, 1 },
+			new byte[]{ 2, 3 }
+		);
+		
+		assertThat( in.readBytes(4), is(new byte[]{ 0, 1, 2, 3}) );
 		assertThat( in.isDone(), is(true) );
 	}
 	
