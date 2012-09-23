296f91ed4c51aa230470ebbd1498893fe6f4dbeb
Douglas Q Hawkins <dougqh@gmail.com>
2012-09-23 19:07:40 -0400

    Adding equals and hashCode for field operations

--- net.dougqh.jak.jvm.core/src/net/dougqh/jak/jvm/operations/GetFieldOperation.java
+++ net.dougqh.jak.jvm.core/src/net/dougqh/jak/jvm/operations/GetFieldOperation.java
@@ -48,4 +48,28 @@ public abstract class GetFieldOperation implements JvmOperation {
 	public final Type[] getStackResultTypes() {
 		return new Type[] { Any.class };
 	}
+	
+	@Override
+	public final int hashCode() {
+		return this.getCode() ^ this.targetType.hashCode() ^ this.field.hashCode();
+	}
+	
+	@Override
+	public boolean equals( final Object obj ) {
+		if ( obj == this ) {
+			return true;
+		} else if ( ! ( obj instanceof GetFieldOperation ) ) {
+			return false;
+		} else {
+			GetFieldOperation that = (GetFieldOperation)obj;
+			return ( this.getCode() == that.getCode() ) &&
+				this.targetType.equals( that.targetType ) &&
+				this.field.equals( that.field );
+		}
+	}
+	
+	@Override
+	public final String toString() {
+		return this.getId() + " " + this.targetType + " " + this.field;
+	}
 }
