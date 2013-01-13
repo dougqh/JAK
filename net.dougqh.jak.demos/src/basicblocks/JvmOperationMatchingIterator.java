e75067c639d2a555e8a4015325b50a01abb99c37
Douglas Q Hawkins <dougqh@gmail.com>
2013-01-13 12:19:01 -0500

    Enough machinery to identify loop bounds

--- net.dougqh.jak.demos/src/basicblocks/JvmOperationMatchingIterator.java
+++ net.dougqh.jak.demos/src/basicblocks/JvmOperationMatchingIterator.java
@@ -6,9 +6,34 @@ import net.dougqh.jak.jvm.operations.JvmOperation;
 
 public final class JvmOperationMatchingIterator implements Iterator<JvmOperation> {
 	private final Iterator<? extends JvmOperation> opIter;
+	private JvmOperation prior;
+	private JvmOperation cur;
 	
 	public JvmOperationMatchingIterator(final Iterator<? extends JvmOperation> opIter) {
 		this.opIter = opIter;
+		
+		this.prior = null;
+		this.cur = null;
+	}
+	
+	public final <T extends JvmOperation> T findNext(final Class<T> opClass) {
+		while ( this.hasNext() ) {
+			T next = this.nextIf(opClass);
+			if ( next != null ) {
+				return next;
+			}
+		}
+		return null;
+	}
+	
+	public final <T extends JvmOperation> T findNext(final JvmOperationMatcher<T> matcher) {
+		while ( this.hasNext() ) {
+			T next = this.nextIf(matcher);
+			if ( next != null ) {
+				return next;
+			}
+		}
+		return null;
 	}
 	
 	@Override
@@ -18,12 +43,56 @@ public final class JvmOperationMatchingIterator implements Iterator<JvmOperation
 	
 	@Override
 	public final JvmOperation next() {
-		return this.opIter.next();
+		this.prior = this.cur;
+		
+		this.cur = this.opIter.next();
+		return this.cur;
 	}
 	
 	public final <T extends JvmOperation> T nextIf(final Class<T> opClass) {
 		JvmOperation op = this.next();
 		if ( opClass.isAssignableFrom(op.getClass()) ) {
+			@SuppressWarnings("unchecked")
+			T castedOp = (T)op;
+			return castedOp;
+		} else {
+			return null;
+		}
+	}
+	
+	public final <T extends JvmOperation> T nextIf(final JvmOperationMatcher<T> matcher) {
+		JvmOperation op = this.next();
+		if ( matcher.matches(op) ) {
+			@SuppressWarnings("unchecked")
+			T castedOp = (T)op;
+			return castedOp;
+		} else {
+			return null;
+		}
+	}
+
+	public final boolean hasPrior() {
+		return ( this.prior != null );
+	}
+	
+	public final JvmOperation prior() {
+		return this.prior;
+	}
+	
+	public final <T extends JvmOperation> T priorIf(final Class<T> opClass) {
+		JvmOperation op = this.prior();
+		if ( opClass.isAssignableFrom(op.getClass()) ) {
+			@SuppressWarnings("unchecked")
+			T castedOp = (T)op;
+			return castedOp;
+		} else {
+			return null;
+		}
+	}
+	
+	public final <T extends JvmOperation> T priorIf(final JvmOperationMatcher<T> matcher) {
+		JvmOperation op = this.prior();
+		if ( matcher.matches(op) ) {
 			@SuppressWarnings("unchecked")
 			T castedOp = (T)op;
 			return castedOp;
--- net.dougqh.jak.demos/test-src/LoopUnrolling.java
+++ net.dougqh.jak.demos/test-src/LoopUnrolling.java
@@ -4,11 +4,14 @@ import net.dougqh.jak.disassembler.JvmMethod;
 import net.dougqh.jak.jvm.operations.ConstantOperation;
 import net.dougqh.jak.jvm.operations.IfOperation;
 import net.dougqh.jak.jvm.operations.LoadOperation;
+import net.dougqh.jak.jvm.operations.StoreOperation;
 import basicblocks.BasicBlock;
 import basicblocks.FlowControl;
+import basicblocks.JvmOperationMatcher;
 import basicblocks.JvmOperationMatchingIterator;
 import basicblocks.Loop;
 import basicblocks.Loops;
+import static basicblocks.JvmOperationMatchers.*;
 
 public final class LoopUnrolling {
 	public static final void main(final String[] args) throws IOException {
@@ -24,24 +27,41 @@ public final class LoopUnrolling {
 	private static final void tryUnroll(final Loop loop) {
 		if ( loop.numBlocks() != 2 ) return;
 		
+		
+		// First analyze the test block to see if it has the relevant bits
 		BasicBlock testBlock = loop.testBlock();
 		if ( testBlock.numOps() != 3 ) return;
 		
-		JvmOperationMatchingIterator iter = testBlock.iterator();
+		JvmOperationMatchingIterator testIter = testBlock.iterator();
 		
-		LoadOperation loadOp = iter.nextIf(LoadOperation.class);
+		LoadOperation loadOp = testIter.nextIf(LOAD);
 		if ( loadOp == null ) return; 
 		
-		ConstantOperation constOp = iter.nextIf(ConstantOperation.class);
-		if ( constOp == null ) return;
+		ConstantOperation limitConstOp = testIter.nextIf(ConstantOperation.class);
+		if ( limitConstOp == null ) return;
 		
-		IfOperation ifOp = iter.nextIf(IfOperation.class);
+		IfOperation ifOp = testIter.nextIf(IfOperation.class);
 		if ( ifOp == null ) return;
 		
+		
 		int loopVarSlot = loadOp.slot();
-		int loopLimit = constOp.<Integer>value();
+		
+		
+		// Second analyze the initialization block
+		JvmOperationMatchingIterator initIter = loop.initBlock().iterator();
+		
+		StoreOperation storeOp = initIter.findNext(store(loopVarSlot));
+		if ( storeOp == null ) return;
+		
+		ConstantOperation initConstOp = initIter.priorIf(CONST);
+		if ( initConstOp == null ) return;
+		
+		
+		int loopInit = initConstOp.<Integer>value();
+		int loopLimit = limitConstOp.<Integer>value();
 		
 		System.out.println("loop var: " + loopVarSlot);
+		System.out.println("init: " + loopInit);
 		System.out.println("limit: " + loopLimit);
 	}
 }
