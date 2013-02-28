071e0882983a4cda25dbc5b8da2d5cfe20fa4af6
Douglas Q Hawkins <dougqh@gmail.com>
2013-02-27 19:25:38 -0500

    Killing whitespace

--- net.dougqh.jak.jvm.disassembler/src/net/dougqh/jak/disassembler/ClassLocator.java
+++ net.dougqh.jak.jvm.disassembler/src/net/dougqh/jak/disassembler/ClassLocator.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public interface ClassLocator {
-	public abstract Iterable< InputStream > list() throws IOException;
+	public abstract Iterable<InputStream> list() throws IOException;
 	
-	public abstract InputStream load( final String className ) throws IOException;
+	public abstract InputStream load(final String className) throws IOException;
 }
