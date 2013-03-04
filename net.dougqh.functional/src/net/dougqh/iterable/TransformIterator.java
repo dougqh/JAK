a8d3ee44a23c14e28109c6183965813a5f4ad4dd
Douglas Q Hawkins <dougqh@gmail.com>
2013-03-03 21:25:33 -0500

    Resurrection the functional iteration package in functional bundle

---
+++ net.dougqh.functional/src/net/dougqh/iterable/TransformIterator.java
@@ -0,0 +1,42 @@
+package net.dougqh.iterable;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public abstract class TransformIterator< I, O > implements Iterator< O > {
+	private final Iterator< ? extends I > iterator;
+	
+	public TransformIterator( final Enumeration< ? extends I > enumeration ) {
+		this( new EnumerationIterator< I >( enumeration ) );
+	}
+
+	public TransformIterator( final I... inputs ) {
+		this( Arrays.asList( inputs ) );
+	}
+	
+	public TransformIterator( final Iterable< ? extends I > iterable ) {
+		this( iterable.iterator() );
+	}
+	
+	public TransformIterator( final Iterator< ? extends I > iterator ) {
+		this.iterator = iterator;
+	}
+	
+	@Override
+	public final boolean hasNext() {
+		return this.iterator.hasNext();
+	}
+	
+	@Override
+	public final O next() {
+		return this.transform( this.iterator.next() );
+	}
+	
+	@Override
+	public final void remove() {
+		throw new UnsupportedOperationException();
+	}
+	
+	protected abstract O transform( final I input );
+}
